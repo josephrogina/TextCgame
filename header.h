6e89700c8246afbaf038a95a5af0b367c2e3f232
Joseph Rogina <joseph@rogina.net>
2013-08-03 13:10:01 -0700

    Create header.h
    This is the header file for the game.

---
+++ header.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define SIZE 30
+
+void door();
+void dresser();
+void mirror();
+void window();
+void hall();
+void hallway();
+void righthall();
+void lefthall();
+void guard();
+void coffeeroom();
+void toilet();
+void secondroom();
+void endhall();
+void deadguard();
+void armory();
+int pepsi();
+int change(int);
+char menu(char);
